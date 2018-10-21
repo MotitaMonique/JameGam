0eafd0866aa1aebc7c36b9474e9be3ff51ae4aa5
Kevin Dolan (RIT Student) <kxd2795@ad.rit.edu>
2018-10-21 12:13:54 -0400

    main menu added, hideous im sorry

---
+++ JameGam/Assets/Scripts/MenuScripts/QuitGame.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+//this script is supposed to exclusively be called by the Quit button *through* the SceneManager empty gameobject
+public class QuitGame : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    //when clicked, close game
+    public void onClick()
+    {
+        Debug.Log("Closing game"); //test code
+        Application.Quit();
+    }
+}
---
+++ JameGam/Assets/Scripts/MenuScripts/StartGame.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartGame : MonoBehaviour {
+
+    //loads the dang game
+    public void LoadGame()
+    {
+        SceneManager.LoadScene("MainGame");
+    }
+}
